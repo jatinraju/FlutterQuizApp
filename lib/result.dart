abfc736fc45ddb0bbc4ff9fc13e1e58442974af3
jatinraju <jatin.raju795@gmail.com>
2022-03-18 14:02:27 +0530

    adding icons

--- lib/result.dart
+++ lib/result.dart
@@ -3,20 +3,26 @@ import 'package:flutter/material.dart';
 class Result extends StatelessWidget {
   final int resultScore;
   final Function resetHandler;
+  var loadImage;
   Result(this.resultScore, this.resetHandler);
 
   String get resultPhrase {
     var resultText;
     if (resultScore == 0) {
       resultText = 'Try to Answer atleast one Question...';
+      loadImage = 'assets/images/thinking.png';
     } else if (resultScore <= 8) {
       resultText = 'You are Awesome and innocent';
+      loadImage = 'assets/images/angel.png';
     } else if (resultScore <= 12) {
       resultText = 'Pretty Likeable!';
+      loadImage = 'assets/images/smile.png';
     } else if (resultScore <= 16) {
       resultText = 'You are .... strange?!';
+      loadImage = 'assets/images/nervous.png';
     } else {
       resultText = 'You are so bad!';
+      loadImage = 'assets/images/dislike.png';
     }
     return resultText;
   }
@@ -31,6 +37,11 @@ class Result extends StatelessWidget {
             resultPhrase,
             style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
             textAlign: TextAlign.center,
+          ),
+          Image.asset(
+            loadImage,
+            width: 200,
+            height: 100,
           ),
           FlatButton(
             onPressed: resetHandler,
